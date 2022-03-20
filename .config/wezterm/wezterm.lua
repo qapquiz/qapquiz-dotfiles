706b795bdc34b8193c4f6ebbffe54a46ac2cdcf2
armariya <arm.armariya@gmail.com>
2022-03-20 18:07:25 +0700

    feat: update config to use mapped keymap and change colorscheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
 return {
 	-- font
-  font = wezterm.font("JetBrains Mono"),
+  font = wezterm.font("Cartograph CF"),
 	font_size = 14.0,
-	line_height = 1.1,
+	-- harfbuzz_features = {"cv01", "cv02", "cv03", "cv10"},
+	line_height = 0.9,
 
 	-- color_scheme
-	color_scheme = "3024 Night",
+	color_scheme = "Catppuccin",
 
 	-- terminal_area
 	window_padding = {
@@ -18,4 +19,14 @@ return {
 
   -- Spawn a fish shell in login mode
   default_prog = {"/usr/local/bin/fish", "-l"},
+
+  keys = {
+    -- disable default assignment 
+    {key="t", mods="SUPER", action="DisableDefaultAssignment"},
+    {key="f", mods="SUPER", action="DisableDefaultAssignment"},
+
+    -- add key bindings
+    {key="mapped:t", mods="SUPER", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+    {key="mapped:f", mods="SUPER", action=wezterm.action{Search={CaseSensitiveString=""}}},
+  }
 }
